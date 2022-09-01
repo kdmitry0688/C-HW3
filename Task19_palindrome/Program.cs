fffd36e16cbc54fd03fa0aaa915035321fb7e606
Dmitriy <kazany.dmitry@gmail.com>
2022-09-01 19:28:36 +0300

    add task polindrome

---
+++ Task19_palindrome/Program.cs
@@ -0,0 +1,17 @@
+﻿string xText;
+Console.WriteLine("Введите пятизначное число: ");
+xText = Console.ReadLine();
+Console.WriteLine( Sunasad(xText));
+
+
+string Sunasad(string xText)
+{
+if ((xText[0] == xText[4]) && (xText[1] == xText[3]))
+ {
+    return ("Введеное число является палиндромом");
+ }
+ else
+ {
+    return "Введеное число не является палиндромом"; 
+ }
+}
