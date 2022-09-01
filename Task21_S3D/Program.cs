5fedba88084c339fee05d093d02d1ceb4752a7a1
Dmitriy <kazany.dmitry@gmail.com>
2022-09-01 19:40:27 +0300

    add task S3D

--- Task21_S3D/Program.cs
+++ Task21_S3D/Program.cs
@@ -1,15 +1,24 @@
-﻿int n;
-Console.WriteLine("Введите число N: ");
-n = Convert.ToInt32(Console.ReadLine());
-kub(n);
-void kub(int n)
-{
-  int index = 1;
-  int kub = 0;
-while (index <= 1);
-{
-  kub = index*index*index;
-  Console.Write(kub + " ");
-  index++;
-}
-}
+﻿int x1 = Coordinate("x", "A");
+ int y1 = Coordinate("y", "A");
+ int z1 = Coordinate("z", "A");
+ int x2 = Coordinate("x", "B");
+ int y2 = Coordinate("x", "B");
+ int z2 = Coordinate("x", "B");
+
+ (int, int, int ) a = (1, 2, 3);
+
+ double rast = Math.Round((Rasstoyanie(x1, y1, z1, x2, y2, z2)), 2);
+ Console.WriteLine($"Длина отрезка {rast}");
+
+ int Coordinate(string coor, string tochka)
+ {
+    Console.Write($"Введите координату {coor} точки {tochka}: ");
+    int vvod = Convert.ToInt32(Console.ReadLine());
+    return vvod;
+ }
+
+double Rasstoyanie (double arg1, double arg2, double arg3, double arg4, double arg5, double arg6)
+ {
+    double answer = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
+    return answer;
+ }
