14f4050fa72c4f1270b85f5acb4a4e598dcf90d0
Dmitriy <kazany.dmitry@gmail.com>
2022-09-01 19:38:41 +0300

    add task kube

---
+++ Task23_ArrN/Program.cs
@@ -0,0 +1,15 @@
+﻿int n;
+Console.WriteLine("Введите число N: ");
+n = Convert.ToInt32(Console.ReadLine());
+kub(n);
+void kub(int n)
+{
+  int index = 1;
+  int kub = 0;
+while (index <= 1);
+{
+  kub = index*index*index;
+  Console.Write(kub + " ");
+  index++;
+}
+}
